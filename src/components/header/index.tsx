797d0b1f85fef6c10d5370940beb0abccb43c62d
Sujin <tnwlsk0190@gmail.com>
2022-06-30 20:31:21 +0900

    feat: 헤더, 푸터 레이아웃 설정

---
+++ src/components/header/index.tsx
@@ -0,0 +1,32 @@
+import { MainPage } from '@pages/MainPage';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../../assets/toteelogo-kr.png';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="content">
+          <Link to="/">
+            <img src={logo} alt="토티 로고" />
+          </Link>
+          <div className="buttonWrapper">
+            <ul>
+              <li>
+                <button className="createButton">스터디 개설</button>
+              </li>
+              <li className="line" />
+              <li>
+                <button className="loginButton">로그인</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
