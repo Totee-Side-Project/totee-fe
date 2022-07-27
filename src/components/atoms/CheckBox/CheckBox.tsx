850798d414b65a8c86294942bfd33213f9c0f907
aeong98 <sy9815@gmail.com>
2022-07-27 21:17:15 +0900

    :sparkles: 카드 hover 애니메이션 효과, 톱니바퀴 드롭다운 디자인, 댓글 작성자만 수정/삭제, editStudy style 수정

--- src/components/atoms/CheckBox/CheckBox.tsx
+++ src/components/atoms/CheckBox/CheckBox.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
-
+import './checkbox.scss';
 interface Props {
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   label: string;
   data:any
+  isChecked:boolean;
 }
 
 export const Checkbox = (props: Props) => {
   return (
     <div>
-      <input
-        type="checkbox"
-        id={props.label}
-        checked={false}
-        onChange={props.handleChange}
-        value={props.data}
-      />{props.data}
+      <label>
+        <input
+          type="checkbox"
+          id={props.label}
+          checked={props.isChecked}
+          onChange={props.handleChange}
+          value={props.data}
+          className="checkbox-input"
+        />
+      <span className="checkbox-label">{props.data}</span>
+      </label>
     </div>
   );
 };
