3ff02631a0136cf80bddf1a4ad817dbe74557b75
YeongMin Ahn <ahn0min95@gmail.com>
2022-11-02 00:47:28 +0900

    Feat: CreateStudy DefaultForm 작업

--- src/components/atoms/Select/SelectItem.tsx
+++ src/components/atoms/Select/SelectItem.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, ReactNode } from 'react';
+import type { ButtonHTMLAttributes, MouseEvent, ReactNode } from 'react';
 
 // html의 button이 받을 수 있는 속성에서 확장한다.
 interface Props extends ButtonHTMLAttributes<HTMLElement> {
@@ -6,6 +6,7 @@ interface Props extends ButtonHTMLAttributes<HTMLElement> {
   right?: ReactNode;
   top?: ReactNode;
   bottom?: ReactNode;
+  onClick?: (e?: MouseEvent<any>) => void;
 }
 
 export function SelectItem({
@@ -14,11 +15,12 @@ export function SelectItem({
   right,
   top,
   bottom,
+  onClick,
   ...props
 }: Props) {
   // children이 아이콘이 될 수 있지 않을까?
   return (
-    <div className="item_wrap">
+    <div className="item_wrap" onClick={onClick}>
       {top}
       <div {...props}>
         {left}
--- src/components/pages/CreateStudyPage/CreateStudyPage.tsx
+++ src/components/pages/CreateStudyPage/CreateStudyPage.tsx
@@ -1,9 +1,15 @@
 // import { EditStudyPage } from '@components/organism';
+import { Banner } from '@components/common';
 import { CreateStudy } from '@components/organism/CreateStudy/CreateStudy';
 // 클릭한 값 보여주기
 
 function CreateStudyPage() {
-  return <CreateStudy />;
+  return (
+    <>
+      <Banner />
+      <CreateStudy />
+    </>
+  );
 }
 
 export default CreateStudyPage;
