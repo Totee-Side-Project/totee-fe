6ea9e5274d697bd7fcc94890847cde4bb7370b33
YeongMin Ahn <ahn0min95@gmail.com>
2022-11-07 23:40:43 +0900

    Test: api url 수정

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -103,12 +103,12 @@ export const AlarmAPI = {
 
 // window.location.host;
 // 로그인 리다이렉트 uri - 우선 local에서 테스트할 수 있게 작업함
+// ? `${window.location.host}/oauth/redirect`
 export const OAUTH2_REDIRECT_URI =
   process.env.NODE_ENV === 'production'
-    ? `${window.location.host}/oauth/redirect`
-    : // 'https://totee-fe-omega.netlify.app/oauth/redirect'
-      // 'http://localhost:4173/oauth/redirect'
-      'http://localhost:3000/oauth/redirect';
+    ? // ? 'http://localhost:4173/oauth2/redirect'
+      `https://${window.location.host}/oauth/redirect`
+    : 'http://localhost:3000/oauth/redirect';
 
 // 구글 로그인
 export const GOOGLE_AUTH_URL =
