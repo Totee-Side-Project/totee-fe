9680eaa8bf79421461d980cfef3931c3073643ae
Sujin <tnwlsk0190@gmail.com>
2022-11-08 22:48:07 +0900

    chore: 스터디 배너 변경

--- src/components/pages/CreateStudyPage/CreateStudyPage.tsx
+++ src/components/pages/CreateStudyPage/CreateStudyPage.tsx
@@ -1,12 +1,14 @@
 // import { EditStudyPage } from '@components/organism';
-import { Banner } from '@components/common';
+// import { Banner } from '@components/common';
+import Banner from '../../../assets/svg/Study Banner.svg';
 import { CreateStudy } from '@components/organism/CreateStudy/CreateStudy';
 // 클릭한 값 보여주기
 
 function CreateStudyPage() {
   return (
     <>
-      <Banner />
+      {/*<Banner />*/}
+      <img src={Banner} alt="스터디 배너" />
       <CreateStudy />
     </>
   );
