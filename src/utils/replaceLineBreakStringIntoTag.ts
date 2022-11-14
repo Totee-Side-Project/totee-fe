ef3a7fc19112ba633add7f83335ded54e1d0a85e
YeongMin Ahn <ahn0min95@gmail.com>
2022-11-14 16:36:13 +0900

    Feat: 게시물 작성 시 유효성 검사 추가

--- src/utils/replaceLineBreakStringIntoTag.ts
+++ src/utils/replaceLineBreakStringIntoTag.ts
@@ -5,7 +5,14 @@ export const replaceLineBreakStringIntoTag = (text: string) => {
 
   textList.forEach((text) => {
     if (text === '') return;
-    result.push(text.replace(/^\n*/g, '').replace(/\n+/g, '<br />'));
+    result.push(text.replace(/^\n*/g, '').replace(/\n+/g, '<br/>'));
   });
   return result;
+};
+
+export const replaceLineBreakTagIntoString = (text: string) => {
+  return text
+    .replace(/<p>/g, '')
+    .replace(/<\/p>/g, '\n\n')
+    .replace(/<br\/>/g, '\n');
 };
