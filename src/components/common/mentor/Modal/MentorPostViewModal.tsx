1a60bc27cbd82c6db8697b157d3780637c4c48ff
sujin <tnwlsk0190@gmail.com>
2022-11-14 18:43:23 +0900

    feat: 멘토링 상세보기 모달 제작중

---
+++ src/components/common/mentor/Modal/MentorPostViewModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MentorPostViewModal.module.scss';
+
+function MentorPostViewModal() {
+  return (
+    <div>
+      <div>모달</div>
+    </div>
+  );
+}
+
+export default MentorPostViewModal;
