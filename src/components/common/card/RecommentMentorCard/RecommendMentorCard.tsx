0396f3eca391931eb15114d4a4491a41542606e7
Sujin <tnwlsk0190@gmail.com>
2022-11-15 00:43:23 +0900

    feat: 멘토링 모달 내부 제작

--- src/components/common/card/RecommentMentorCard/RecommendMentorCard.tsx
+++ src/components/common/card/RecommentMentorCard/RecommendMentorCard.tsx
@@ -1,35 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './recommendMentorCard.scss';
 
 export default function RecommendMentorCard() {
   return (
     <div>
-    <div className="mento_card_container">
-        <div className="mento_card_header">
+      <div>
+        <div className="mento_card_container">
+          <div className="mento_card_header">
             <h1 className="mento_card_title">C++ 및 게임 프로그래밍 멘토</h1>
             <div className="mento_card_description_wrapper">
-            <div className="mento_card_description">
+              <div className="mento_card_description">
                 <span>분야</span>
                 <span>개발/프로그래밍</span>
-            </div>
-            <div className="mento_card_description">
+              </div>
+              <div className="mento_card_description">
                 <span>경력</span>
                 <span>3~5년차 현직자</span>
+              </div>
             </div>
-            </div>
-        </div>
-        <div className="mento_card_line"></div>
-        <div className="mento_card_body">
-        미국 뉴욕의 테크 회사에서 프로덕트 디자이너로 활동 중인 에릭입니다. UX, UI디자인, 유저 리서치...
-        </div>
-        <div className="mento_card_footer">
+          </div>
+          <div className="mento_card_line"></div>
+          <div className="mento_card_body">
+            미국 뉴욕의 테크 회사에서 프로덕트 디자이너로 활동 중인 에릭입니다.
+            UX, UI디자인, 유저 리서치...
+          </div>
+          <div className="mento_card_footer">
             <div className="mento_card_profile_image"></div>
             <div className="mento_card_nickname">닉네임</div>
-            <div className='mento_v_line'></div>
+            <div className="mento_v_line"></div>
             <div className="mento_card_time">일주일 1회</div>
+          </div>
         </div>
-
+      </div>
     </div>
-    </div>
-  )
+  );
 }
--- src/components/common/mentor/Modal/MentorPostViewModal.tsx
+++ src/components/common/mentor/Modal/MentorPostViewModal.tsx
@@ -1,11 +1,60 @@
+import classes from './MentorPostViewModal.module.scss';
+import { MentorModal } from './MentorModal/MentorModal';
 import React from 'react';
 import './MentorPostViewModal.module.scss';
 
-function MentorPostViewModal() {
+interface IMentorPostViewModalProps {
+  isOpen: boolean;
+  setIsOpen: (e: boolean) => void;
+}
+
+function MentorPostViewModal({ isOpen, setIsOpen }: IMentorPostViewModalProps) {
   return (
-    <div>
-      <div>모달</div>
-    </div>
+    <MentorModal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <div className={classes.mentorModal}>
+        <div className={classes.headerTitle}>멘토링 소개</div>
+        <div className={classes.topLine} />
+        <div className={classes.contentWrapper}>
+          <div className={classes.title}>
+            [N년 경력자 출신] 개발자 취업 관련 상담
+          </div>
+          <div className={classes.profile}>
+            <img className={classes.img} />
+            <div className={classes.name}>모각코고인물</div>
+          </div>
+          <div className={classes.detail}>
+            <div className={classes.position}>직무</div>
+            <div>SW 엔지니어</div>
+          </div>
+          <div className={classes.detail}>
+            <div className={classes.career}>경력</div>
+            <div>미들(3-4년차)</div>
+          </div>
+          <div className={classes.price}>1회 멘토링 1시간 / 11,000원</div>
+          <div className={classes.contentLine} />
+          <div className={classes.content}>
+            인도하겠다는 끝까지 피어나는 구하기 우리의 말이다. 커다란 인류의
+            피는 없는 반짝이는 없으면, 이성은 그들은 같이, 사막이다. 능히 웅대한
+            피어나는 이것이다. 목숨이 거선의 무엇을 같은 사막이다. 열락의 우리는
+            사랑의 되려니와, 이것을 속에서 관현악이며, 실로 사막이다.
+            인도하겠다는 끝까지 피어나는 구하기 우리의 말이다. 커다란 인류의
+            피는 없는 반짝이는 없으면, 이성은 그들은 같이, 사막이다. 능히 웅대한
+            피어나는 이것이다. 목숨이 거선의 무엇을 같은 사막이다. 열락의 우리는
+            사랑의 되려니와, 이것을 속에서 관현악이며, 실로 사막이다.
+            인도하겠다는 끝까지 피어나는 구하기 우리의 말이다. 커다란 인류의
+            피는 없는 반짝이는 없으면, 이성은 그들은 같이, 사막이다. 능히 웅대한
+            피어나는 이것이다. 목숨이 거선의 무엇을 같은 사막이다. 열락의 우리는
+            사랑의 되려니와, 이것을 속에서 관현악이며, 실로 사막이다.
+            인도하겠다는 끝까지 피어나는 구하기 우리의 말이다. 커다란 인류의
+            피는 없는 반짝이는 없으면, 이성은 그들은 같이, 사막이다. 능히 웅대한
+            피어나는 이것이다. 목숨이 거선의 무엇을 같은 사막이다. 열락의 우리는
+            사랑의 되려니와, 이것을 속에서 관현악이며, 실로 사막이다.
+          </div>
+        </div>
+        <div className={classes.bottomLine} />
+        <div className={classes.btn}>신청하기</div>
+      </div>
+    </MentorModal>
   );
 }
 
