c6016ae6c7c7e448cb93537b50cd45d5ca2b0b83
YeongMin Ahn <ahn0min95@gmail.com>
2022-11-15 18:43:13 +0900

    Feat: 스터디 지원하기 기능 추가

--- src/components/common/join/Modal/JoinModal.tsx
+++ src/components/common/join/Modal/JoinModal.tsx
@@ -1,49 +1,65 @@
-import React from 'react';
+import React, { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
 import { Button, Modal } from '@components/atoms';
 import './joinModal.scss';
+import { useParams } from 'react-router-dom';
+import { useDeleteApplicant, useUpdateApplicant } from '@hooks/useMutateQuery';
+import { useRecoilValue } from 'recoil';
+import { UserState } from '@store/user';
 
 interface IJoinModalProps {
   isOpen: boolean;
-  setIsOpen: (e: boolean) => void;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 export function JoinModal({ isOpen, setIsOpen }: IJoinModalProps) {
+  const useProfile = useRecoilValue(UserState);
+  const [formData, setFormData] = useState('');
+  const { id } = useParams();
+  const addApplicantQuery = useUpdateApplicant(id);
+  const applyApplicationQuery = useDeleteApplicant(id);
+
+  const addApplicationOnClick = () => {
+    addApplicantQuery.mutateAsync(formData).then((response) => {
+      if (response.status === 200) {
+        setIsOpen((pre) => !pre);
+        alert('성공');
+        return;
+      }
+      return alert('실패했습니다.');
+    });
+  };
+  const onChangeByTextarea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+    setFormData(value);
+  };
+
   return (
-    <>
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <div className="ApplyModalWrapper">
-          {/*<section className="ApplyModalWrapper">*/}
-          <div className="ApplyTitle">지원 하기</div>
-          <div className="SubTitle">지원하실 포지션과 소개를 적어주세요.</div>
-          <div>
-            <div
-              className="ApplyProfile"
-              // style={{
-              //   backgroundRepeat: 'no-repeat',
-              //   backgroundSize: 'cover',
-              // }}
-            ></div>
-            <div className="ApplyName">닉네임</div>
-            <div className="ApplyEmail">dlapdlf@gmail.com</div>
-            <textarea
-              className="ApplyIntro"
-              placeholder="본인에 대한 짧은 소개입니다. 본인에 대한 짧은 소개입니다. 본인에 대한 짧은 소개입니다. 본인에 대한 짧은 소개입니다."
-            />
-            <Button
-              text="완료"
-              style={{
-                color: '#fff',
-                width: '210px',
-                backgroundColor: '#568A35',
-                margin: '30px auto 0 auto',
-              }}
-            ></Button>
-            {/*<a style={{ display: 'none' }}></a>*/}
-            {/*<a style={{ display: 'none' }}></a>*/}
-          </div>
-          {/*</section>*/}
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <div className="ApplyModalWrapper">
+        <div className="ApplyTitle">지원 하기</div>
+        <div className="SubTitle">지원하실 포지션과 소개를 적어주세요.</div>
+        <div>
+          <div className="ApplyProfile"></div>
+          <div className="ApplyName">{useProfile.nickname}</div>
+          <div className="ApplyEmail">{useProfile.email}</div>
+          <textarea
+            className="ApplyIntro"
+            placeholder="본인에 대한 짧은 소개입니다. 본인에 대한 짧은 소개입니다. 본인에 대한 짧은 소개입니다. 본인에 대한 짧은 소개입니다."
+            value={formData}
+            onChange={onChangeByTextarea}
+          />
+          <Button
+            text="완료"
+            style={{
+              color: '#fff',
+              width: '210px',
+              backgroundColor: '#568A35',
+              margin: '30px auto 0 auto',
+            }}
+            onClick={addApplicationOnClick}
+          />
         </div>
-      </Modal>
-    </>
+      </div>
+    </Modal>
   );
 }
