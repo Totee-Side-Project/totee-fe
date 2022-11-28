349841decd0164ca2b45dcc9fa8ee74dea41e2ea
YeongMin Ahn <ahn0min95@gmail.com>
2022-11-28 19:38:15 +0900

    Style: 상세페이지 타이틀 말줄임, 연락링크 스타일 변경

--- src/components/pages/DetailPage/NewDetailPage.tsx
+++ src/components/pages/DetailPage/NewDetailPage.tsx
@@ -432,20 +432,21 @@ const SectionContactView = ({
   postData: IResponsePostDetail;
 }) => {
   return (
-    <div style={{ margin: '30px 5%', textAlign: 'left' }}>
+    <div className={classes.contact_view_wrap}>
+      {/* <div style={{ margin: '30px 5%', textAlign: 'left' }}> */}
       <p>
-        진행지역 :
+        진행지역{' : '}
         {!postData.detailedRegion ? '온라인으로 진행' : postData.detailedRegion}
       </p>
       <p>
-        연락방식 :
-        <Link
-          to={postData.contactLink}
+        연락방식{' : '}
+        <a
+          href={postData.contactLink}
           target="_blank"
           rel="noopener noreferrer"
         >
-          {postData.contactLink}
-        </Link>
+          {postData.contactMethod}
+        </a>
       </p>
     </div>
   );
