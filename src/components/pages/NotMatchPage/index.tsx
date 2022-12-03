0362a95cdd5c26a13cd44e78c2199ef22cba1944
YeongMin Ahn <ahn0min95@gmail.com>
2022-12-04 04:42:48 +0900

    Feat: 라우팅 표를 탐색하여 가이드메시지를 다르게 안내하게 작업

--- src/components/pages/NotMatchPage/index.tsx
+++ src/components/pages/NotMatchPage/index.tsx
@@ -1,16 +1,35 @@
-import { useNavigate } from 'react-router-dom';
+import { isLoginRoutes } from 'App';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import classes from './style.module.scss';
-
 interface Props {
   status: string;
 }
 
 // 여기서 라우팅 표에 들어가 있다면 case1, 그렇지 않으면 case2
-export const NotMatchPage = ({ status }: Props) => {
-  if (status === 'loading') return <Lodaing />;
-  if (status === 'error') return <NotMatch />;
+const NotMatchPage = ({ status }: Props) => {
+  const [isNotMatch, setIsNotMatch] = useState(true);
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
-  return <NotMatch />;
+  useEffect(() => {
+    if (isLoginRoutes.find(({ path }) => path === pathname))
+      setIsNotMatch(false);
+  }, []);
+
+  if (status === 'loading') return <Lodaing />;
+  // if (status === 'error') return <NotMatch />;
+
+  return (
+    <div className={classes.NotMatchContainer}>
+      <h1 className={classes.Title}>
+        {isNotMatch ? '올바르지 않은 경로입니다.' : '로그인이 필요합니다.'}
+      </h1>
+      <button className={classes.NavButton} onClick={() => navigate('/')}>
+        홈으로
+      </button>
+    </div>
+  );
 };
 
 export const Lodaing = () => {
@@ -21,14 +40,16 @@ export const Lodaing = () => {
   );
 };
 
-export const NotMatch = () => {
-  const navigate = useNavigate();
-  return (
-    <div className={classes.NotMatchContainer}>
-      <h1 className={classes.Title}>올바르지 않은 경로입니다.</h1>
-      <button className={classes.NavButton} onClick={() => navigate('/')}>
-        홈으로
-      </button>
-    </div>
-  );
-};
+// export const NotMatch = () => {
+//   const navigate = useNavigate();
+//   return (
+//     <div className={classes.NotMatchContainer}>
+//       <h1 className={classes.Title}>올바르지 않은 경로입니다.</h1>
+//       <button className={classes.NavButton} onClick={() => navigate('/')}>
+//         홈으로
+//       </button>
+//     </div>
+//   );
+// };
+
+export default NotMatchPage;
