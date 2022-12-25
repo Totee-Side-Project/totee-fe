f05aa50a998f736f78396ad8e37e82600a30ee71
YeongMin Ahn <ahn0min95@gmail.com>
2022-12-25 12:13:31 +0900

    Remove: atoms/Input을 SearchInput으로 변경하고 ui/Input을 atoms/Input으로 대체

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,13 +1,17 @@
 export * from './Counter';
 export * from './Circle/Circle';
+export * from './Label/Label';
 export * from './Image/ImageWithWebp';
 export * from './Modal/Modal';
 export * from './Button/Button';
 export * from './Input/Input';
+export * from './SearchInput/SearchInput';
 export * from './Alert/Alert';
 export * from './CategoryButton/CategoryButton';
 export * from './Line/Line';
 export * from './Icon/Icon';
 export * from './CheckBox/CheckBox';
+
 export * from './SectionTitle/SectionTitle';
 export * from './Select/SelectItem';
+export * from './ScrollTopButton/ScrollTopButton';
