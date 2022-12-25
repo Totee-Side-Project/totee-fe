0f4fbd6ba02074ef03cdcce7c2bec0324c55e8e0
YeongMin Ahn <ahn0min95@gmail.com>
2022-12-25 15:51:50 +0900

    Chore: CheckBox Component 재추가

---
+++ src/components/atoms/CheckBox/CheckBox.tsx
@@ -0,0 +1,83 @@
+import type { ReactNode } from 'react';
+import { useEffect, useState } from 'react';
+
+import { Circle } from '@components/atoms';
+import classes from './checkBox.module.scss';
+
+interface Props {
+  top?: ReactNode;
+  isChecked?: string;
+  options: string[];
+  onClick: (data: string) => void;
+  className: string;
+}
+// single mode만 지원하는 상태
+// 다수모드에서는?
+// isCheckedId type이 array가 되면 될 것 같다.
+export const Checkbox = ({
+  top,
+  isChecked,
+  options,
+  onClick,
+  className,
+}: Props) => {
+  const [isCheckeds, setIsCheckeds] = useState(
+    options.map((option, index) => ({ id: option, isChecked: false })),
+  );
+  const [isCheckedId, setIsCheckedId] = useState(!isChecked ? '' : isChecked);
+  const handleClick = (payload: string) => {
+    if (isCheckedId === payload) {
+      setIsCheckedId('');
+      onClick('');
+      return;
+    }
+    // onClick(options[payload]);
+    onClick(payload);
+    setIsCheckedId(payload);
+  };
+  useEffect(() => {
+    setIsCheckeds((oldState) =>
+      oldState.map(({ id }) => ({
+        id,
+        isChecked: isCheckedId === id ? true : false,
+      })),
+    );
+  }, [isCheckedId]);
+  return (
+    <>
+      {top}
+      {options.map((option, idx) => (
+        <span className={className} key={option + idx}>
+          <span className={classes.checkbox_wrap}>
+            <input
+              className={classes.checkbox}
+              type="checkbox"
+              id={`check_${option}`}
+            ></input>
+            <Trigger
+              isSelected={isCheckeds[idx].isChecked}
+              option={option}
+              onClick={() => handleClick(option)}
+            />
+          </span>
+        </span>
+      ))}
+    </>
+  );
+};
+const Trigger = ({
+  option,
+  isSelected,
+  onClick,
+}: {
+  option: ReactNode;
+  isSelected: boolean;
+  onClick: () => void;
+}) => {
+  return (
+    <span className={classes.label_wrap} onClick={onClick}>
+      <Circle selected={isSelected} backgroundColor="#7BA364" />
+      <label htmlFor={`check_${option}`}>{option}</label>
+    </span>
+  );
+};
