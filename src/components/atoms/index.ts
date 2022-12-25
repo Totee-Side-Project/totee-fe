679ccd7fdb421df73ecccbfe6d5e3dd48e9fdbef
YeongMin Ahn <ahn0min95@gmail.com>
2022-12-25 11:53:26 +0900

    Remove: ui/ImageWithWebp 를 atoms 폴더 내부로 이동

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,6 @@
 export * from './Counter';
 export * from './Circle/Circle';
+export * from './Image/ImageWithWebp';
 export * from './Modal/Modal';
 export * from './Button/Button';
 export * from './Input/Input';
