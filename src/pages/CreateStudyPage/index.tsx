366308d92d49e4566c9db27be93c63dd384ed632
YeongMin Ahn <ahn0min95@gmail.com>
2022-12-25 15:33:41 +0900

    Fix: createStudy 폴더 대소문자 변경

--- src/pages/CreateStudyPage/index.tsx
+++ src/pages/CreateStudyPage/index.tsx
@@ -1,4 +1,4 @@
-import { CreateStudy } from '@components/domains/CreateStudy/CreateStudy';
+import { CreateStudy } from '@components/domains/createStudy/CreateStudy';
 // 클릭한 값 보여주기
 
 const CreateStudyPage = () => {
