4a04d56cf2b76f6842992e75f2cc032effebc91b
YeongMin Ahn <ahn0min95@gmail.com>
2022-12-27 19:11:33 +0900

    Chore: SectionTitle 전체보기 버튼의 유무의 판단을 외부로 돌림(props 추가)

--- src/components/atoms/SectionTitle/SectionTitle.tsx
+++ src/components/atoms/SectionTitle/SectionTitle.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import classes from './SectionTitle.module.scss';
 import { ReactComponent as ChevronIcon } from '@assets/svg/chevron-down.svg';
+import classes from './SectionTitle.module.scss';
 
 interface Props {
   title: string;
   sub: string;
   description: string;
+  isTotal: boolean;
   onClickTotal?: () => {};
 }
-export function SectionTitle({ title, sub, description, onClickTotal }: Props) {
+export function SectionTitle({
+  title,
+  sub,
+  description,
+  isTotal,
+  onClickTotal,
+}: Props) {
   return (
     <div className={classes.title_wrapper}>
       <div>
@@ -16,10 +22,12 @@ export function SectionTitle({ title, sub, description, onClickTotal }: Props) {
         <h2 className={classes.title_main}>{title}</h2>
         <div className={classes.title_description}>{description}</div>
       </div>
-      <div className={classes.total_button_wrapper}>
-        <div className={classes.button_desc}>전체 보기</div>
-        <ChevronIcon />
-      </div>
+      {isTotal && (
+        <div className={classes.total_button_wrapper} onClick={onClickTotal}>
+          <div className={classes.button_desc}>전체 보기</div>
+          <ChevronIcon />
+        </div>
+      )}
     </div>
   );
 }
