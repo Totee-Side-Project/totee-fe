8842d39511218331b0b51eba7f7dfc1cc7a5ef2d
YeongMin Ahn <ahn0min95@gmail.com>
2022-12-29 20:27:17 +0900

    Style: PostsFilter 스타일 변경

--- src/components/domains/posts/PostsFilter/index.tsx
+++ src/components/domains/posts/PostsFilter/index.tsx
@@ -1,14 +1,9 @@
-import {
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  useEffect,
-  useState,
-} from 'react';
+import { Dispatch, ReactNode, SetStateAction, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { Circle } from '@components/atoms';
 import { IResponsePostDetail } from 'types/api.types';
+import { useSort } from '@hooks/useSort';
 import classes from './postsFilter.module.scss';
 
 interface Props {
@@ -39,13 +34,9 @@ export const PostsFilter = ({ datas, setDatas, options, element }: Props) => {
 
   // 🟠 Todo: inlineStyle을 SCSS로 변경 예정
   return (
-    <ul style={{ display: 'flex', gap: 10 }}>
+    <ul className={classes.filters}>
       {Object.entries(options).map(([key, value]) => (
-        <li
-          style={{ display: 'flex', cursor: 'pointer' }}
-          key={value}
-          onClick={() => onClick(key)}
-        >
+        <li key={key} className={classes.filter} onClick={() => onClick(key)}>
           <Circle
             selected={searchParams.get('filter') === key}
             options={{ outCircle: false }}
@@ -55,69 +46,4 @@ export const PostsFilter = ({ datas, setDatas, options, element }: Props) => {
       ))}
     </ul>
   );
-};
-
-type useSortPropsType = IResponsePostDetail[];
-
-// 🟠 Todo: hooks 폴더 내부로 이동시킬 예정
-export const useSort = (posts: useSortPropsType = []) => {
-  const [sortedDatas, setSortedDatas] = useState<any[]>(posts);
-
-  // posts를 직접적으로 호출해주지 않기 때문이 아닐까?
-  // 빈배열이 넘어오더라도 결국 component에서 data가 있을 때에만 해당 hooks를 호출해주도록 한다면 문제는 없을 것 같다.;
-
-  // setSortFunctions
-  const sortRecent = () => {
-    return [...posts].sort(
-      (left, right) =>
-        Number(new Date(right.createdAt)) - Number(new Date(left.createdAt)),
-    );
-  };
-  const sortCommentCount = () => {
-    return [...posts].sort((left, right) => right.commentNum - left.commentNum);
-  };
-  const sortLikeCount = () => {
-    return [...posts].sort((left, right) => right.likeNum - left.likeNum);
-  };
-  const sortViewCount = () => {
-    return [...posts].sort((left, right) => right.view - left.view);
-  };
-
-  // sortFunctions
-  const setSortRecent = () => {
-    const result = sortRecent();
-    setSortedDatas(result);
-  };
-  const setSortCommentCount = () => {
-    const result = sortCommentCount();
-    setSortedDatas(result);
-  };
-  const setSortLikeCount = () => {
-    const result = sortLikeCount();
-    setSortedDatas(result);
-  };
-  const setSortViewCount = () => {
-    const result = sortViewCount();
-    setSortedDatas(result);
-  };
-
-  const sortFunctions = {
-    recent: sortRecent,
-    comment: sortCommentCount,
-    like: sortLikeCount,
-    view: sortViewCount,
-  };
-
-  const setSortFunctions = {
-    recent: setSortRecent,
-    comment: setSortCommentCount,
-    like: setSortLikeCount,
-    view: setSortViewCount,
-  } as { [key: string]: () => void };
-
-  // setsortFunctions, sortFunctions, sortedDatas를 return 받는다.
-  // 값만 리턴받기를 원한다면 sortFuctions를 사용하면 되고 setState까지 원한다면 setSortFunctions를 사용하면 된다.
-  // 해당 컴포넌트에서 state를 필요로 할 경우에도 사용가능하고, 해당 컴포넌트에서 props setState를 전달받으면 부모의 state까지 변경해줄 수 있다.
-
-  return { sortedDatas, setSortedDatas, setSortFunctions, sortFunctions };
 };
