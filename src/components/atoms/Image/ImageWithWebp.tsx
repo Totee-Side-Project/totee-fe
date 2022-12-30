8647c7b516d7ba47776766557e9e260e362d293a
YeongMin Ahn <ahn0min95@gmail.com>
2022-12-30 17:12:28 +0900

    Feat/i123/changeLogo : Banner 반응형 적용 (#125)
    * Replace: Totee 로고 변경
    
    * Chore: 로그아웃시 센터에서 Swal alert가 뜨도록 변경
    
    * Chore: ImaveWithWebp Component props 변경
    
    * Chore: SectionTitle 전체보기 버튼의 유무의 판단을 외부로 돌림(props 추가)
    
    * Feat: Banner를 반응형으로 변경
    
    * Chore: SectionTitle Component의 isTotal props를 optional로 변경

--- src/components/atoms/Image/ImageWithWebp.tsx
+++ src/components/atoms/Image/ImageWithWebp.tsx
@@ -2,14 +2,16 @@ import { ImgHTMLAttributes } from 'react';
 
 interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   media?: string;
+  src: string;
+  alt: string;
   srcSet: string;
 }
 
-export const ImageWithWebp = ({ media, srcSet, ...props }: Props) => {
+export const ImageWithWebp = ({ media, src, alt, srcSet, ...props }: Props) => {
   return (
     <picture>
       <source type="image/webp" media={media} srcSet={srcSet} />
-      <img {...props} />
+      <img src={src} alt={alt} {...props} />
     </picture>
   );
 };
