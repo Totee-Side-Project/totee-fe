786da4be3223d552089fbdeab0142548dfc6d568
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2023-01-16 22:23:08 +0900

    Feat: 멘토링 글 모달 Props 정의 및 UI 로직

--- src/components/common/mentoring/MentoringPostDetailModal/index.tsx
+++ src/components/common/mentoring/MentoringPostDetailModal/index.tsx
@@ -1,48 +1,64 @@
 import classes from './index.module.scss';
 import { ReactComponent as XIcon } from '@assets/svg/xicon.svg';
+import { MouseEventHandler } from 'react';
+import { IMentoring } from 'types/api.types';
 
-const data = {
-  title: '[3년차 개발자 출신] 개발자 취업 관련 상담',
-  mentor: {
-    name: '고인물',
-  },
-  content: `LLLLLLLLLLorem ipsum dolor sit amet consectetur adipisicing elit. Provident recusandae consectetur, odio excepturi nostrum fugit? Culpa eaque esse animi id magnam. Suscipit dolorum voluptate facilis? Ipsam laborum quis voluptas porro!
-    `,
-};
+interface MentoringPostDetailModalProps {
+  isOpen: boolean;
+  onCloseClick: MouseEventHandler;
+  onApplyClick: MouseEventHandler;
+  mentoring: IMentoring;
+}
 
-function MentoringPostDetailModal() {
+function MentoringPostDetailModal({
+  isOpen,
+  mentoring,
+  onCloseClick,
+  onApplyClick,
+}: MentoringPostDetailModalProps) {
+  if (!isOpen) {
+    return null;
+  }
+
+  const { title, content, cost, career, profileImageUrl, nickname, field } =
+    mentoring;
+
+  const formattedCost = Intl.NumberFormat('ko-KR').format(cost);
+
   return (
     <div className={classes.overlay}>
       <div className={classes.modal}>
         <div className={classes.header}>
           <h2>멘토링 소개</h2>
-          <XIcon className={classes.close_icon} />
+          <XIcon className={classes.close_icon} onClick={onCloseClick} />
         </div>
 
         <div className={classes.body}>
-          <h1>{data.title}</h1>
+          <h1>{title}</h1>
           <div className={classes.profile}>
-            <img src="" alt="" />
-            <span className={classes.profile_name}>{data.mentor.name}</span>
+            <img src={profileImageUrl} alt={nickname} />
+            <span className={classes.profile_name}>{nickname}</span>
             <div className={classes.mentor_badge}>Totee</div>
           </div>
           <div className={classes.profile_details}>
             <div className={classes.profile_detail}>
               <span>직무</span>
-              <span>SW 엔지니어</span>
+              <span>{field}</span>
             </div>
             <div className={classes.profile_detail}>
               <span>경력</span>
-              <span>미들 (3-4년차)</span>
+              <span>{career}</span>
             </div>
           </div>
-          <div className={classes.cost}>1회 멘토링 1시간 / 11,000원</div>
+          <div className={classes.cost}>
+            1회 멘토링 1시간 / {formattedCost}원
+          </div>
           <div className={classes.divider} />
-          <div className={classes.content}>{data.content}</div>
+          <div className={classes.content}>{content}</div>
         </div>
 
         <div className={classes.bottom_fixed}>
-          <button>신청하기</button>
+          <button onClick={onApplyClick}>신청하기</button>
         </div>
       </div>
     </div>
