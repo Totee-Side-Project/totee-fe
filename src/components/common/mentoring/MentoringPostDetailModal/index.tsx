6b30c6517d609b4790af2d8617c5c09151aba988
최진우 <chlwlsdn0828@gmail.com>
2023-01-17 21:47:27 +0900

    Feat: 멘토링 글 상세보기 모달 구현 (#141)
    * Feat: 멘토링 글 모달 UI 구현
    
    * Feat: 멘토링 글 모달 Props 정의 및 UI 로직
    
    * Feat: 멘토링 글 모달 overlay 클릭 시 모달 종료
    
    * Feat: 멘토링 글 모달 관리 로직 구현

---
+++ src/components/common/mentoring/MentoringPostDetailModal/index.tsx
@@ -0,0 +1,69 @@
+import classes from './index.module.scss';
+import { ReactComponent as XIcon } from '@assets/svg/xicon.svg';
+import { MouseEventHandler } from 'react';
+import { IMentoring } from 'types/api.types';
+
+interface MentoringPostDetailModalProps {
+  onCloseClick(): void;
+  onApplyClick(): void;
+  mentoring: IMentoring;
+}
+
+function MentoringPostDetailModal({
+  mentoring,
+  onCloseClick,
+  onApplyClick,
+}: MentoringPostDetailModalProps) {
+  const { title, content, cost, career, profileImageUrl, nickname, field } =
+    mentoring;
+
+  const formattedCost = Intl.NumberFormat('ko-KR').format(cost);
+
+  const onOverlayClick: MouseEventHandler = (e) => {
+    if (e.target === e.currentTarget) {
+      e.stopPropagation();
+      onCloseClick();
+    }
+  };
+
+  return (
+    <div className={classes.overlay} onClick={onOverlayClick}>
+      <div className={classes.modal}>
+        <div className={classes.header}>
+          <h2>멘토링 소개</h2>
+          <XIcon className={classes.close_icon} onClick={onCloseClick} />
+        </div>
+
+        <div className={classes.body}>
+          <h1>{title}</h1>
+          <div className={classes.profile}>
+            <img src={profileImageUrl} alt={nickname} />
+            <span className={classes.profile_name}>{nickname}</span>
+            <div className={classes.mentor_badge}>Totee</div>
+          </div>
+          <div className={classes.profile_details}>
+            <div className={classes.profile_detail}>
+              <span>직무</span>
+              <span>{field}</span>
+            </div>
+            <div className={classes.profile_detail}>
+              <span>경력</span>
+              <span>{career}</span>
+            </div>
+          </div>
+          <div className={classes.cost}>
+            1회 멘토링 1시간 / {formattedCost}원
+          </div>
+          <div className={classes.divider} />
+          <div className={classes.content}>{content}</div>
+        </div>
+
+        <div className={classes.bottom_fixed}>
+          <button onClick={onApplyClick}>신청하기</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MentoringPostDetailModal;
