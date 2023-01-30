9f28aa6449e3ea160e1239588b69c470f24cd999
ahn0min <ahn0min95@gmail.com>
2023-01-30 16:23:18 +0900

    Chore: Buttons 폴더명을 Button으로 변경

--- src/components/common/login/LoginOauth.tsx
+++ src/components/common/login/LoginOauth.tsx
@@ -14,16 +14,11 @@ function LoginOauth() {
 
   localStorage.setItem('loginData', obj);
 
-  // login token 담길시 새로고침
-  // 0min: 새로 고침이 아니라 url만 변경해 주는 것
   useEffect(() => {
-    // document.location.href = '/';
     navigate('/');
   }, [login]);
 
-  // Link tag를 반환할 필요가 있는가?
   return null;
-  // return <Link to="/">메인으로 이동</Link>;
 }
 
 export default LoginOauth;
