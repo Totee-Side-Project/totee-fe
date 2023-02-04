f43bed6dfbdc30a9a3441d3758291854459d440c
olo90632951 <olo90632951@gmail.com>
2023-02-04 18:45:21 +0900

    Feat: MentoringPostDetailModal props 변경

--- src/components/common/mentoring/MentoringPostDetailModal/index.tsx
+++ src/components/common/mentoring/MentoringPostDetailModal/index.tsx
@@ -7,12 +7,14 @@ interface MentoringPostDetailModalProps {
   onCloseClick(): void;
   onApplyClick?(): void;
   mentoring?: IMentoringPost;
+  isAuthorizedUser: boolean;
 }
 
 function MentoringPostDetailModal({
   mentoring,
   onCloseClick,
   onApplyClick,
+  isAuthorizedUser,
 }: MentoringPostDetailModalProps) {
   if (!mentoring) {
     return <></>;
@@ -63,7 +65,9 @@ function MentoringPostDetailModal({
         </div>
 
         <div className={classes.bottom_fixed}>
-          <button onClick={onApplyClick}>신청하기</button>
+          <button disabled={!isAuthorizedUser} onClick={onApplyClick}>
+            {isAuthorizedUser ? '신청하기' : '로그인 후 신청할 수 있습니다'}
+          </button>
         </div>
       </div>
     </div>
--- src/components/domains/myPage/contentSection/mentoring/FavoriteMentoringList/index.tsx
+++ src/components/domains/myPage/contentSection/mentoring/FavoriteMentoringList/index.tsx
@@ -2,9 +2,12 @@ import { IMentoringPost } from '@api/mentoring/types';
 import MentoringPostDetailModal from '@components/common/mentoring/MentoringPostDetailModal';
 import CardsSection from '@components/domains/myPage/common/CardsSection';
 import { useGetLikedMentoringPosts } from '@hooks/query/useGetQuery';
+import { loginState } from '@store/login';
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 
 const FavoriteMentoringList = () => {
+  const [loginData] = useRecoilState(loginState);
   const { data: posts } = useGetLikedMentoringPosts();
 
   const [currentPost, setCurrentPost] = useState<IMentoringPost>();
@@ -29,6 +32,7 @@ const FavoriteMentoringList = () => {
         <MentoringPostDetailModal
           mentoring={currentPost}
           onCloseClick={() => setIsOpenedMentoringPostDetailModal(false)}
+          isAuthorizedUser={loginData.state}
         />
       )}
     </>
